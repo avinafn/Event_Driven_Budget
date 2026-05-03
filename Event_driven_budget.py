c723ee83a83844bd389945fc3258029512cfd8f9
avinafn <aallafchian@yahoo.com>
2026-05-03 00:53:23 -0700

    first commit

---
+++ Event_driven_budget.py
@@ -0,0 +1,76 @@
+class ExpenseRequest:
+    def init(self, description: str, department: str ,amount: float, priority: int):
+        self.description = description
+        self.department = department
+        self.amount = amount
+        self.priority = priority
+
+    def rep(self):
+        return f"[Priority: {self.priority}] {self.department} - {self.description}: ${self.amount}"
+
+
+class HeapManagment:
+    def init(self):
+        self.heap = []
+    
+    def parent(self, index):
+        return (index - 1) // 2
+
+    def leftChild(self, index):
+        return (index * 2) + 1
+
+    def rightChild(self, index):
+        return (index * 2) + 2
+
+    #helper function for heapifyUp
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def insert(self, request: ExpenseRequest):
+        self.heap.append(request)
+        self.heapifyUp(len(self.heap) - 1)
+
+    #moves the inserted object up if we are not at a root and the object's priotiy is higher than its parent's
+    def heapifyUp(self, index):
+
+        parentIndex = self.parent(index)
+
+        if index > 0 and self.heap[index].priority > self.heap[parentIndex].priority:
+            self.swap(index, parentIndex)
+            #recursively check the new position compared to the new parent
+            heapifyUp(parentIndex)
+
+    def removeMax(self):
+        if len(self.heap) == 0:
+            return None
+
+        maxVal = self.heap[0]
+
+        lastItem = self.heap.pop()
+
+        if len(self.heap) > 0:
+            self.heap[0] = lastItem
+            self.heapifyDown(0)
+        
+        return maxVal
+
+
+    def heapifyDown(self, index):
+        largest = index
+        leftIndex = self.leftChild(index)
+        rightIndex = self.rightChild(index)
+
+        if leftIndex < len(self.heap) and self.heap[largest].priority < self.heap[leftIndex].priority:
+            largest = leftIndex
+
+        if rightIndex < len(self.heap) and self.heap[largest].priority < self.heap[rightIndex].priority:
+            largest = rightIndex
+
+        if largest != index:
+            self.swap(index, largest)
+            self.heapifyDown(largest)
+            
+            
+
+
+
---
+++ api_server.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from core_engine import ExpenseRequest, HeapManagment
